7fad2a6415c470e65da6e4bffb506cf01cb3f02b
VeliAliyev <veli_aliyev@outlook.com>
2022-12-07 21:34:28 +0300

    initial Commit

---
+++ src/main/java/com/velialiyev/webflux_rest/WebFluxRestApplication.java
@@ -0,0 +1,13 @@
+package com.velialiyev.webflux_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFluxRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/velialiyev/webflux_rest/WebFluxRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velialiyev.webflux_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
