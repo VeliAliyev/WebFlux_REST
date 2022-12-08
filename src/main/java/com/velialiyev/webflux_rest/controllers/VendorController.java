aca80e4b3ad87587fada895a4dc3f685f6608911
VeliAliyev <veli_aliyev@outlook.com>
2022-12-08 19:09:59 +0300

    VendorController post put patch added

--- src/main/java/com/velialiyev/webflux_rest/controllers/VendorController.java
+++ src/main/java/com/velialiyev/webflux_rest/controllers/VendorController.java
@@ -3,9 +3,8 @@ package com.velialiyev.webflux_rest.controllers;
 import com.velialiyev.webflux_rest.domain.Vendor;
 import com.velialiyev.webflux_rest.repository.VendorRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.reactivestreams.Publisher;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -22,5 +21,29 @@ public class VendorController {
     @GetMapping("/{id}")
     Mono<Vendor> getVendorById(@PathVariable String id){
         return vendorRepository.findById(id);
+    }
+
+    @PostMapping
+    public Mono<Void> createVendor(@RequestBody Publisher<Vendor> vendorPublisher){
+        return vendorRepository.saveAll(vendorPublisher).then();
+    }
+
+    @PutMapping("/{id}")
+    public Mono<Vendor> updateVendor(@PathVariable String id, @RequestBody Vendor vendor){
+        vendor.setId(id);
+        return vendorRepository.save(vendor);
+    }
+
+    @PatchMapping("/{id}")
+    public Mono<Vendor> patchVendor(@PathVariable String id, @RequestBody Vendor vendor){
+        Vendor foundVendor = vendorRepository.findById(id).block();
+
+        if(!foundVendor.getFirstName().equals(vendor.getFirstName())){
+            foundVendor.setFirstName(vendor.getFirstName());
+        }
+        if(!foundVendor.getLastName().equals(vendor.getLastName())){
+            foundVendor.setLastName(vendor.getLastName());
+        }
+        return vendorRepository.save(foundVendor);
     }
 }
